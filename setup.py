b783dfeebdfb9859ebff17e2c57ebc5c8e753b61
ivanmarkov1997 <ivanmarkov1997@gmail.com>
2019-04-06 00:11:54 +0300

    refactor

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ setuptools.setup(
 	name='tlbo-imarkov',
 	version='0.0.1',
 	author='Ivan Markov',
-	author_email='thiendio@yandex.ru',
+	author_email='ivanmarkov1997@gmail.com',
 	description='tearching learning based algorithm',
 	long_description=long_description,
 	long_description_content_type='text/markdown',
 	url='https://github.com/ivanmarkov97/teaching-learning-based-algorithm',
 	packages=setuptools.find_packages(),
 	classifiers=[
-		"Programming Language :: Python :: 2",
+	"Programming Language :: Python :: 2",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
 	]
